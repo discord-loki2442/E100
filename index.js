26b3c7557a4fb1d0cefc64a0ef43c5b6dbe07297
Loki2442 <kul.swas321tik@gmail.com>
2021-03-18 09:26:09 +0530

    Added Main Files

---
+++ index.js
@@ -0,0 +1,21 @@
+const Eris = require("eris");
+const { prefix, token } = require('./config.json');
+ 
+var bot = new Eris(token);
+// Replace BOT_TOKEN with your bot account's token
+ 
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
+});
+ 
+bot.on("messageCreate", (msg) => { // When a message is created
+    if(msg.content === `${prefix}ping`) { // If the message content is "!ping"
+        bot.createMessage(msg.channel.id, "Pong!");
+        // Send a message in the same channel with "Pong!"
+    } else if(msg.content === `${prefix}pong`) { // Otherwise, if the message is "!pong"
+        bot.createMessage(msg.channel.id, "Ping!");
+        // Respond with "Ping!"
+    }
+});
+ 
+bot.connect(); // Get the bot to connect to Discord
